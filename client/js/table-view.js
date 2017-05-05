be627c8a9270d013e5845ea14b120029ab94e03a
azmeer <azmeer.salleh@gmail.com>
2017-05-05 13:15:01 -0700

    Added unit test for updating current cell on formula bar change

--- client/js/table-view.js
+++ client/js/table-view.js
@@ -79,10 +79,6 @@ class TableView {
     this.formulaBarEl.addEventListener('keyup', this.handleFormulaBarChange.bind(this));
   }
   
-  isColumnHeaderRow(row) {
-    return row < 1;
-  }
-
   handleFormulaBarChange(evt) {
     const value = this.formulaBarEl.value;
     this.model.setValue(this.currentCellLocation, value);
@@ -93,11 +89,8 @@ class TableView {
     const col = evt.target.cellIndex;
     const row = evt.target.parentElement.rowIndex - 1;
 
-    if (!this.isColumnHeaderRow(row)) {
-      this.currentCellLocation = { col: col, row: row };
-      this.renderTableBody();
-    }
-
+    this.currentCellLocation = { col: col, row: row };
+    this.renderTableBody();
     this.renderFormulaBar();
   }
   
